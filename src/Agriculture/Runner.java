c2d02dae9e5febaf9590a41064716fc3a3b23c38
dsillman2000 <dsillman2000@gmail.com>
2016-01-30 17:06:51 -0800

    Filled in skeletons for Runner.java and Screen.java

--- src/Agriculture/Runner.java
+++ src/Agriculture/Runner.java
@@ -1,7 +1,17 @@
 package Agriculture;
-
+import javax.swing.JFrame;
 /**
  * Created by Home on 1/30/16.
  */
 public class Runner {
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Agricultural Optimizer");
+        Screen s = new Screen();
+        frame.add(s);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        s.animate(2);
+    }
 }
--- src/Agriculture/Screen.java
+++ src/Agriculture/Screen.java
@@ -1,7 +1,40 @@
 package Agriculture;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by Home on 1/30/16.
  */
-public class Screen {
+public class Screen extends JPanel{
+    BufferedImage buffered;
+    public Screen()
+    {
+
+    }
+    public Dimension getPreferredSize()
+    {
+        return new Dimension(800,600);
+    }
+    public void paintComponent(Graphics g)
+    {
+        if(buffered==null)
+        {
+            buffered = (BufferedImage)(createImage(getWidth(),getHeight()));
+        }
+        Graphics gBuff = buffered.createGraphics();
+        //PAINTING
+
+        //NO MORE PAINTING
+        g.drawImage(buffered,0,0,null);
+    }
+
+    public void animate(int rate)
+    {
+        try {
+            Thread.sleep(rate);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
